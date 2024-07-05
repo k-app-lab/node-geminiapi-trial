33049df14d89125fc533c4cf131192fad4fea4d2
kota.tonami <gintama.saicoo@gmail.com>
2024-07-05 05:20:42 +0000

    geminiAPIを試した

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # node-geminiapi-trial
 NodejsからGenimiAPIを使用してみる
+
+## 参考サイト
+
+- [NodeからGemini APIを使ってみる](https://dev.classmethod.jp/articles/node-gemini-api/)
+- [Gemini 1.0 Pro プレビュー版の利用制限にまつわるエラー](https://qiita.com/7shi/items/54e45181052c914a4d45)
---
+++ main.js
@@ -0,0 +1,40 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+async function run() {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  const prompt = `カラオケで歌う曲を以下の好みに基づいて推薦してください。
+- 好きなジャンル: 1980年代のロック
+- 好きなアーティスト: クイーン、エアロスミス
+- 雰囲気: みんなで盛り上がれる曲
+- 過去の選曲: 
+
+回答は以下のJSON形式でrecommendedSongsの配列要素に10個回答してください。
+
+[回答形式]
+{
+  "recommendedSongs": [
+    {
+      "songTitle": "string",  // 曲の名称を出力してください
+      "artistName": "string",  // アーティスト名を出力してください
+      "reason": "string",  // 選曲理由を出力してください
+      "referenceURL": "string"  // 参考URLは曲をGoogleで検索したときのURLとしてください
+    }
+  ]
+}
+`;
+
+  const result = await model.generateContentStream(prompt);
+
+  let text = '';
+  for await (const chunk of result.stream) {
+    const chunkText = chunk.text();
+    console.log(chunkText);
+    text += chunkText;
+  }
+}
+
+run();
